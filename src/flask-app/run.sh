52059fcb70542293e777120e2a4a9527b7144394
Enitan (Emmanuel) Meduteni <enitanm@github.com>
2025-11-29 15:01:45 -0500

    testing script for security scans and unit tests

--- src/flask-app/run.sh
+++ src/flask-app/run.sh
@@ -15,11 +15,8 @@ VERSION="latest"
 
 #Build Docker image
 docker build -t $APP_NAME:$VERSION .
-#Scan Docker image for vulnerabilities
-docker scout cves $APP_NAME:$VERSION --output report.txt
+# #Scan Docker image for vulnerabilities
+# docker scout cves $APP_NAME:$VERSION --output report.txt
 
-#SBOM
-docker scout sbom $APP_NAME:$VERSION --output sbom.txt
-
-run the app
-python app.py
+# #SBOM
+# docker scout sbom $APP_NAME:$VERSION --output sbom.txt
